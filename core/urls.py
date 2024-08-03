28945abf267f92bb0e8796e67be5497dee59b578
Aslon <arslon@gmail.com>
2024-08-04 01:29:35 +0500

    Commit

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
@@ -13,4 +14,4 @@ urlpatterns = [
     path('order/', include("order.urls")),
     path('payment/', include("payment.urls")),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
