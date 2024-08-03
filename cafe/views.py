5088ef8fa444a41eac0ab600644b1528b607b953
Aslon <arslon@gmail.com>
2024-08-03 12:33:28 +0500

    Commit

--- cafe/views.py
+++ cafe/views.py
@@ -6,7 +6,7 @@ from .serializers import ProductSerializer
 from .pagination import CustomPageNumberPagination
 from .permissions import IsAuthorOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.permissions import IsAdminUser
 
 # class ProductListView(generics.ListAPIView):
 #     queryset = Product.objects.all()
@@ -16,16 +16,23 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly | IsAdminUser]
 
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = CustomPageNumberPagination
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly | IsAdminUser]
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['category']
+    filterset_fields = ['category', "available"]
     search_fields = ['name', 'description']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        discount_not_null = self.request.query_params.get('discount_not_null')
+        if discount_not_null:
+            queryset = queryset.exclude(discount=None)
+        return queryset
 
 
