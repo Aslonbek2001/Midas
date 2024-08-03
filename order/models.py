22cbc1ec9e13cb2a5a27b1a9604d54ff5f6599d9
Aslon <arslon@gmail.com>
2024-08-03 16:16:06 +0500

    Commit

--- order/models.py
+++ order/models.py
@@ -26,10 +26,20 @@ class Order(models.Model):
         ('completed', 'Completed'),
         ('canceled', 'Canceled'),
     ]
+    PAYMENT_CHOICES = [
+        ('card', 'card'),
+        ('cash', 'Cash'),
+    ]
+    PAYMENT_STATUS = [
+        ('paid', 'Paid'),
+        ('unpaid', 'unpaid'),
+    ]
     user = models.ForeignKey(ClientModel, on_delete=models.CASCADE, related_name="orders")
     quantity = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    payment_type = models.CharField(max_length=4, choices=PAYMENT_CHOICES, default='card')
+    payment_status = models.CharField(max_length=6, choices=PAYMENT_STATUS, default='unpaid')
     order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
