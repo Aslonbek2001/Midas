17d379f6e93ef8ef5a78f8dd18d5edb4e023dc16
Aslon <arslon@gmail.com>
2024-08-05 06:00:45 +0500

    Commit

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PaymentView, CombinedOrderPaymentView
 
 urlpatterns = [
-    path('api/payment/', PaymentView.as_view(), name='payment'),
-    path('api/check', CombinedOrderPaymentView.as_view(), name='payment'),
+    # path('api/payment/', PaymentView.as_view(), name='payment'),
+    # path('api/check', CombinedOrderPaymentView.as_view(), name='payment'),
 
 ]
